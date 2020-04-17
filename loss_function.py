725744045d80fa9cdd7e158c5028c22b9d32f3c8
mabernstein <31412233+mabernstein@users.noreply.github.com>
2020-04-17 11:44:06 -0500

    checks

--- loss_function.py
+++ loss_function.py
@@ -13,6 +13,9 @@ def cross_entropy(input, target, weight=None, size_average=True):
     input = input.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
     target = target.view(-1)
 
+    print(input.size())
+    print(target.size())
+
     #Loss Function
     loss = F.cross_entropy(
         input, target, weight=weight, size_average=size_average, ignore_index=250
