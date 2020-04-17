f609d03df99023406d0271c968764a95286c0b50
mabernstein <31412233+mabernstein@users.noreply.github.com>
2020-04-17 12:56:38 -0500

    add multi cross entropy

--- loss_function.py
+++ loss_function.py
@@ -15,7 +15,7 @@ def cross_entropy(input, target, weight=None, size_average=True):
     print(target.size())
 
     input = input.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
-    target = target.view(-1,4)
+    target = target.view(-1)
 
     print(input.size())
     print(target.size())
@@ -24,4 +24,25 @@ def cross_entropy(input, target, weight=None, size_average=True):
     loss = F.cross_entropy(
         input, target, weight=weight, size_average=size_average, ignore_index=250
     )
+    return loss
+
+
+def multi_scale_cross_entropy2d(input, target, weight=None, size_average=True, scale_weight=None):
+    if not isinstance(input, tuple):
+        return cross_entropy2d(input=input, target=target, weight=weight, size_average=size_average)
+
+    # Auxiliary training for PSPNet [1.0, 0.4] and ICNet [1.0, 0.4, 0.16]
+    if scale_weight is None:  # scale_weight: torch tensor type
+        n_inp = len(input)
+        scale = 0.4
+        scale_weight = torch.pow(scale * torch.ones(n_inp), torch.arange(n_inp).float()).to(
+            target.device
+        )
+
+    loss = 0.0
+    for i, inp in enumerate(input):
+        loss = loss + scale_weight[i] * cross_entropy2d(
+            input=inp, target=target, weight=weight, size_average=size_average
+        )
+
     return loss
