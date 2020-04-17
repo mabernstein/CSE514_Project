5ff70c4cf48e992752e72ddba0d5f5f13d91e036
mabernstein <31412233+mabernstein@users.noreply.github.com>
2020-04-17 12:06:24 -0500

    test

--- loss_function.py
+++ loss_function.py
@@ -11,8 +11,8 @@ def cross_entropy(input, target, weight=None, size_average=True):
         print("Inconsistent Input and Target Size -- Adjust")
         input = F.interpolate(input, size=(ht, wt), mode="bilinear", align_corners=True)
 
-    input = input.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
-    target = target.view(-1)
+    # input = input.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+    # target = target.view(-1)
 
     print(input.size())
     print(target.size())
